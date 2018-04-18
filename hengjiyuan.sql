df4792dfc2c3d750c06cdab19e664b742d69f8f1
DESKTOP-O7GGBE8\海洋 <424138799@qq.com>
2018-04-18 17:19:56 +0800

    no message

--- hengjiyuan.sql
+++ hengjiyuan.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2018-04-17 18:31:41
+Date: 2018-04-18 17:19:31
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -32,11 +32,13 @@ CREATE TABLE `hj_admin_company` (
   `describe` varchar(500) DEFAULT NULL COMMENT '描述',
   `createTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of hj_admin_company
 -- ----------------------------
+INSERT INTO `hj_admin_company` VALUES ('2', '123', '对方的', 'の22', '分公司', '3242', '234', '434', null, '222', '2018-04-18 10:10:35');
+INSERT INTO `hj_admin_company` VALUES ('3', '中原n', '342', '234', '分公司', '3434', '3434', '3434', null, '234', '2018-04-18 10:11:39');
 
 -- ----------------------------
 -- Table structure for hj_admin_department
@@ -55,11 +57,12 @@ CREATE TABLE `hj_admin_department` (
   `createTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `remarks` varchar(500) DEFAULT NULL COMMENT '说明',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of hj_admin_department
 -- ----------------------------
+INSERT INTO `hj_admin_department` VALUES ('1', '2', '32424', 'cnsk点卡积分', 'dsf', 'IT', '1', '234', '23', '2018-04-18 10:57:20', '333333');
 
 -- ----------------------------
 -- Table structure for hj_admin_user
@@ -73,7 +76,7 @@ CREATE TABLE `hj_admin_user` (
   `phone` varchar(11) DEFAULT NULL COMMENT '手机号码',
   `telPhone` varchar(20) DEFAULT NULL COMMENT '电话',
   `email` varchar(30) DEFAULT NULL COMMENT '邮箱',
-  `g_id` int(10) DEFAULT NULL COMMENT '用户所在用户组',
+  `gId` int(10) DEFAULT NULL COMMENT '用户所在用户组',
   `companyId` int(10) DEFAULT NULL COMMENT '所属公司id',
   `department` int(10) DEFAULT NULL COMMENT '部门id',
   `duty` varchar(500) DEFAULT NULL COMMENT '职责',
@@ -82,13 +85,14 @@ CREATE TABLE `hj_admin_user` (
   `loginTime` datetime DEFAULT NULL COMMENT '最后登陆时间',
   `remarks` varchar(500) DEFAULT NULL COMMENT '说明',
   `password` varchar(32) DEFAULT NULL COMMENT '密码',
+  `userCode` varchar(20) DEFAULT NULL COMMENT '用户工号',
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of hj_admin_user
 -- ----------------------------
-INSERT INTO `hj_admin_user` VALUES ('1', 'admin', '系统创建者', '1', '1528277232', null, null, '1', null, null, '34', '1', '0000-00-00 00:00:00', null, '', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `hj_admin_user` VALUES ('1', 'admin', '系统创建者', '1', '1528277232', null, null, '1', null, null, '34', '1', '0000-00-00 00:00:00', null, '', 'e10adc3949ba59abbe56e057f20f883e', null);
 
 -- ----------------------------
 -- Table structure for hj_admin_user_group
@@ -120,7 +124,7 @@ CREATE TABLE `hj_bank` (
   `remarks` varchar(50) DEFAULT NULL COMMENT '备注',
   `createTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of hj_bank
@@ -254,7 +258,7 @@ CREATE TABLE `hj_system_log` (
   `log_message` text,
   `log_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of hj_system_log
@@ -273,3 +277,20 @@ INSERT INTO `hj_system_log` VALUES ('11', 'Bank/addBankUser', '1', '系统创建
 INSERT INTO `hj_system_log` VALUES ('12', 'Bank/addBankUser', '1', '系统创建者', '::1', '1', '新增银行管理人员成功,银行编号是：2,银行管理人员名称是：456654', '2018-04-17 16:20:09');
 INSERT INTO `hj_system_log` VALUES ('13', 'Bank/editBankUser', '1', '系统创建者', '::1', '1', '编辑银行管理人员成功,银行编号是：2,银行管理人员名称是：从', '2018-04-17 17:00:59');
 INSERT INTO `hj_system_log` VALUES ('14', 'Bank/delBankUser', '1', '系统创建者', '::1', '1', '删除银行管理人员成功,银行人员编号是：1', '2018-04-17 17:31:08');
+INSERT INTO `hj_system_log` VALUES ('15', 'Member/AddCompany', null, null, '::1', '1', '新增公司信息成功,公司名称是46464', '2018-04-18 09:54:54');
+INSERT INTO `hj_system_log` VALUES ('16', 'Member/editCompany', null, null, '::1', '1', '新增公司信息成功,公司名称是中原公司', '2018-04-18 10:06:25');
+INSERT INTO `hj_system_log` VALUES ('17', 'Member/editCompany', null, null, '::1', '1', '修改公司信息成功,公司名称是中原公司', '2018-04-18 10:07:01');
+INSERT INTO `hj_system_log` VALUES ('18', 'Member/editCompany', '1', '系统创建者', '::1', '1', '修改公司信息成功,公司名称是中原公司', '2018-04-18 10:07:53');
+INSERT INTO `hj_system_log` VALUES ('19', 'Member/AddCompany', '1', '系统创建者', '::1', '1', '新增公司信息成功,公司名称是123', '2018-04-18 10:10:35');
+INSERT INTO `hj_system_log` VALUES ('20', 'Member/delCompany', '1', '系统创建者', '::1', '1', '修改公司信息成功,公司名称是', '2018-04-18 10:10:44');
+INSERT INTO `hj_system_log` VALUES ('21', 'Member/AddCompany', '1', '系统创建者', '::1', '1', '新增公司信息成功,公司名称是中原n', '2018-04-18 10:11:39');
+INSERT INTO `hj_system_log` VALUES ('22', 'Member/addDepartment', '1', '系统创建者', '::1', '1', '新增部门信息成功,部门名称是cnsk点卡积分', '2018-04-18 10:57:20');
+INSERT INTO `hj_system_log` VALUES ('23', 'Member/editDepartment', '1', '系统创建者', '::1', '1', '新增部门信息成功,部门名称是cnsk点卡积分', '2018-04-18 11:16:16');
+INSERT INTO `hj_system_log` VALUES ('24', 'Member/editDepartment', '1', '系统创建者', '::1', '1', '新增部门信息成功,部门名称是cnsk点卡积分', '2018-04-18 11:16:16');
+INSERT INTO `hj_system_log` VALUES ('25', 'Member/editDepartment', '1', '系统创建者', '::1', '1', '新增部门信息成功,部门名称是cnsk点卡积分', '2018-04-18 11:17:52');
+INSERT INTO `hj_system_log` VALUES ('26', 'Member/addDepartment', '1', '系统创建者', '::1', '1', '新增部门信息成功,部门名称是34534', '2018-04-18 11:21:07');
+INSERT INTO `hj_system_log` VALUES ('27', 'Member/delDepartment', '1', '系统创建者', '::1', '1', '删除部门信息成功,部门id是2', '2018-04-18 11:21:12');
+INSERT INTO `hj_system_log` VALUES ('28', 'Member/AddUser', '1', '系统创建者', '::1', '1', '新增用户信息成功,用户登陆账户是admin12', '2018-04-18 16:14:15');
+INSERT INTO `hj_system_log` VALUES ('29', 'Member/EditUser', '1', '系统创建者', '::1', '1', '编辑用户成功,用户id是:2', '2018-04-18 17:10:48');
+INSERT INTO `hj_system_log` VALUES ('30', 'Member/EditUser', '1', '系统创建者', '::1', '1', '编辑用户成功,用户id是:2', '2018-04-18 17:11:04');
+INSERT INTO `hj_system_log` VALUES ('31', 'Member/delUser', '1', '系统创建者', '::1', '1', '删除用户成功,用户id是:2,用户名称是王姐', '2018-04-18 17:17:14');
